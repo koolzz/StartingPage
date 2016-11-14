c2c211427b29d1027500f2eeba8487fb777a8273
Kolya Yurchenko <urchennko@gmail.com>
2016-11-14 00:19:37 -0500

    work on to-do checklist

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,32 @@
-$(function() {
+$().ready(function() {
   //$('body').css('background-image', 'url("' + "background.jpg" + '")');
   updateTime();
+  updateLinks();
+
+  load();
+  $('#list').keydown(function (event) {
+     var keypressed = event.keyCode || event.which;
+     if (keypressed == 13) {
+         saveChanges();
+     }
+ });
 
 });
+function updateLinks(){
+    var link = [
+        ["top-l","https://calendar.google.com/calendar/render"],
+        ["top-r","https://www.reddit.com/r/funny/"],
+        ["mid-l","https://www.facebook.com/"],
+        ["mid","https://github.com/"],
+        ["mid-r","https://vk.com/feed"],
+        ["bot-l","https://mail.google.com/mail/u/0/#inbox"],
+        ["bot-r","https://blackboard.gwu.edu/webapps/login/"],
+    ]
+    for(i=0;i<link.length;i++){
+        $("."+link[i][0]).attr("href",link[i][1]);
+    }
 
+}
 function updateTime() {
 
   var date = new Date();
@@ -19,6 +42,28 @@ function updateTime() {
 
   setTimeout(updateTime, 400);
 }
+
+function saveChanges() {
+  // Get a value saved in a form.
+
+  var keywords = $.trim($("textarea").val());
+
+    chrome.storage.sync.set({'channels': keywords});
+
+}
+function load() {
+    var channels = "";
+    chrome.storage.sync.get('channels', function (result) {
+        channels = result.channels;
+        //alert(result.channels);
+        if(!channels)
+            $("#list").val("empty");
+        else {
+            $("#list").val(channels);
+        }
+    });
+}
+
 
 function updateWeather() {
   $.simpleWeather({
